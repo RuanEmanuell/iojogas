e28cc0832a4c08ed4d86591b4443334c10316252
Ruan Emanuell Alencar <ruanvitorcom540@gmail.com>
2025-10-24 14:38:43 -0300

    feat: adding sockets and game rooms

---
+++ frontend/src/utils/socket.tsx
@@ -0,0 +1,8 @@
+import { createContext, useContext } from "react";
+import type { Socket } from "socket.io-client";
+
+export const SocketContext = createContext<Socket | null>(null);
+
+export function useSocket() {
+  return useContext(SocketContext);
+}
