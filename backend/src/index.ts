ed0d3ab5ace99e67d05e7d23f508783b149083e2
Ruan Emanuell Alencar <ruanvitorcom540@gmail.com>
2025-10-24 12:52:01 -0300

    feat: adding sockets

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,16 @@
 import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from "socket.io";
 
 const app = express();
 const PORT = 3000;
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"]
+  }
+});
 
 app.use(express.json());
 
@@ -9,6 +18,13 @@ app.get('/', (req, res) => {
   res.send('Servidor Express com TypeScript 🚀');
 });
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+  socket.on("createRoom", (roomName) => {
+    socket.join(roomName);
+    console.log(roomName);
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,10 +1,37 @@
+import { useEffect, useRef } from "preact/hooks";
+import { io, Socket } from "socket.io-client";
 import './app.css';
-import { FiveWords } from './games/fivewords/FiveWords';
-
+import { getRandomNumber } from "./utils/getRandomNumber";
 
 export function App() {
+  const socket = useRef<Socket>();
+
+  function createGame() {
+    if (!socket.current) return;
+
+    const roomId = getRandomNumber(1000, 9999).toString();
+
+    socket.current.emit("createRoom", roomId);
+
+    console.log("🟢 Sala criada:", roomId);
+  }
+
+  useEffect(() => {
+    socket.current = io("http://localhost:3000");
+
+    socket.current.on("connect", () => {
+      console.log("✅ Conectado ao servidor!", socket.current?.id);
+    });
+
+    return () => {
+      socket.current?.disconnect();
+    };
+  }, []);
 
   return (
-    <FiveWords></FiveWords>
-  )
+    <div class="flex flex-col justify-center items-center h-screen bg-gray-900 text-white">
+      <h1 class="text-6xl font-bold my-4">IOJOGAS 🎮❓</h1>
+      <button class="w-64 h-24 bg-green-600 text-3xl font-bold rounded-md cursor-pointer" onClick={createGame}>NOVO JOGO</button>
+    </div>
+  );
 }
